e66ed1ad95c581c0ac74c155ac2f4363d9f20d44
shekhar <shekharpatle999@gmail.com>
2021-04-13 23:07:56 +0530

    Added: add new contact to address book

--- addressbook.java
+++ addressbook.java
@@ -1,6 +1,10 @@
 package com.addressbook;
 
-public class Contact {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class AddContact {
 
 	String firstName;
 	String lastName;
@@ -9,27 +13,43 @@ public class Contact {
 	String email;
 	int zip;
 	int phoneNo;
-	
-	public Contact (String firstName, String lastName, String city, String state, String email, int zip, int phoneNo) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.city = city;
-		this.state = state;
-		this.email = email;
-		this.zip = zip;
-		this.phoneNo = phoneNo;
-	} 
-	
-		
-	public void Display() {
-		System.out.println("First Name: " + this.firstName);
-		System.out.println("Last Name: " + this.lastName);
-		System.out.println("City: " + this.city);
-		System.out.println("State: " + this.state);
-		System.out.println("Zip: " + this.zip);
-		System.out.println("Phone Number: " + this.phoneNo);
-		System.out.println("EmailId: " + this.email);
-		
+	public void NewContact() {
+
+		Scanner newData = new Scanner(System.in);
+		List storeContact = new ArrayList();
+
+		System.out.println("Add to new Contact to Address Book");
+		for (int i = 0; i < 2; i++) {
+			System.out.println("First Name: ");
+			String firstName = newData.next();
+			storeContact.add(firstName);
+
+			System.out.println("Last Name: ");
+			String lastName = newData.next();
+			storeContact.add(lastName);
+
+			System.out.println("City: ");
+			String city = newData.next();
+			storeContact.add(city);
+
+			System.out.println("State: ");
+			String state = newData.next();
+			storeContact.add(state);
+
+			System.out.println("EmailID: ");
+			String email = newData.next();
+			storeContact.add(email);
+
+			System.out.println("Zip: ");
+			int zip = newData.nextInt();
+			storeContact.add(zip);
+
+			System.out.println("PhoneNumber: ");
+			int phoneNo = newData.nextInt();
+			storeContact.add(phoneNo);
+			
+			System.out.println("FirstName: "+firstName+"\nLastName: "+lastName+"\nCity: "+city+"\nState: "+state+"Email: "+email+"\nZip: "+zip+"\nPhone Number: "+phoneNo);
+		}	System.out.println(storeContact);
 	}
 
 }
